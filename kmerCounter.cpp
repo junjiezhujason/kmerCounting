7105209f7428680b213dc5123e5677fb77ffa6dc
Junjie Zhu <junjie.zhu.jason@gmail.com>
2015-04-24 23:01:35 -0700

    run on server

--- kmerCounter.cpp
+++ kmerCounter.cpp
@@ -19,18 +19,15 @@ int main(int argc, char* argv[]){
 
 
     // TEST
-    uint64_t stop = 300000000;
+    //uint64_t stop = 300000000;
     uint64_t total_length = 0;
 
     while (!reader.eos) { 
         //reader.printKmers(); // print for testing
-        if (reader.pos % 10000000 == 0) {
-            printf("%lld bases read...\n", (long long int) reader.pos);
-        }
+        //if (reader.pos % 10000000 == 0) {
+        //    printf("%lld bases read...\n", (long long int) reader.pos);
+        //}
         total_length ++;
-        if (total_length == stop) {
-            break;
-        }
 
         if (!reader.is_ambiguous) { // only consider non-ambigous kmers in the map
             if (MapUni.count(reader.kmer) == 0) { // if the kmer has not been encountered before
@@ -50,6 +47,10 @@ int main(int argc, char* argv[]){
     }
     const char MapUniName[] = "uniquePos";
     unimap_to_file(fastaFname, length, MapUni, MapUniName); // save counts map to .fahist file
+    
+    printf("- total length:\t%lld\n",static_cast<long long>(total_length));
+    printf("- distinct kmers that uniquely appear: \t%d\n", (int) MapUni.size());
+
     //printMapInfo(MapUni); // print map for testing
 
     //const char fahistName[] = "unicorn.fa_30uniquePos";
